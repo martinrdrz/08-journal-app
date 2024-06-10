90324b021cbe95e8bc0ee83e2f7fb3094fd280b6
martinrdrz <martinrdrz@hotmail.com>
2024-06-10 18:02:48 -0300

    parte estetica completa

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,6 +1,6 @@
 import { Box, Toolbar } from '@mui/material';
-import { NavBar } from '../components/NavBar';
-import { SideBar } from '../components/SideBar';
+import { NavBar } from '../components';
+import { SideBar } from '../components';
 
 const drawerWidth = 240;
 
@@ -11,7 +11,7 @@ export const JournalLayout = ({ children }) => {
 
             <SideBar drawerWidth={drawerWidth} />
 
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
                 <Toolbar />
 
                 {children}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,31 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NothingSelectedView } from '../views/NothingSelectedView';
+import { NoteView } from '../views/NoteView';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            {/*<Typography>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatem, aspernatur consequuntur,
-                delectus cumque iure dolor totam ea molestiae facilis velit ducimus, laudantium aperiam consectetur quas
-                adipisci qui sequi quaerat!
-            </Typography>*/}
+            {/*<Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Typography>*/}
+
             <NothingSelectedView />
+
+            {/*<NoteView />*/}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout>
     );
 };
