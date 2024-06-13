3b29a166f4253e69d590c4d6cbcb568743351a3c
martinrdrz <martinrdrz@hotmail.com>
2024-06-13 14:09:48 -0300

    implementado autenticacion

--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -5,17 +5,17 @@ export const NothingSelectedView = () => {
     return (
         <Grid
             container
-            spancing={0}
-            direction='column'
-            alignItems='center'
-            justifyContent='center'
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
             sx={{ minHeight: 'calc(100vh - 120px)', backgroundColor: 'primary.main', borderRadius: 3 }}
         >
-            <Grid item sx={12}>
+            <Grid item xs={12}>
                 <StarOutline sx={{ fontSize: 100, color: 'white' }} />
             </Grid>
-            <Grid item sx={12}>
-                <Typography color='white' variant='h5'>
+            <Grid item xs={12}>
+                <Typography color="white" variant="h5">
                     Selecciona o crea una entrada
                 </Typography>
             </Grid>
